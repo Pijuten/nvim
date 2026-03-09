6e4ba64473e8e5a59b26be4eb973b7fa1d8fd966
stani <stanislaus.cichocki@nobile.energy>
2026-03-09 21:40:01 +0100

    added neogit

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,11 @@
+return {
+  "NeogitOrg/neogit",
+  opts = {},
+  keys = {
+    { "<leader>gn", "<cmd>Neogit<cr>", desc = "Neogit" },
+    { "<leader>gc", "<cmd>Neogit commit<cr>", desc = "Neogit Commit" },
+    { "<leader>gp", "<cmd>Neogit push<cr>", desc = "Neogit Push" },
+    { "<leader>gl", "<cmd>Neogit pull<cr>", desc = "Neogit Pull" },
+    { "<leader>gb", "<cmd>Neogit branch<cr>", desc = "Neogit Branch" },
+  },
+}
