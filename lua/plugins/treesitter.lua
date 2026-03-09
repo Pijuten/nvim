a7bcc415aa46ea6f2edf722059a858c770faf013
stani <stanislaus.cichocki@nobile.energy>
2026-03-09 21:37:33 +0100

    added stuff

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,5 @@
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    vim.list_extend(opts.ensure_installed, {
-      "lua", "vim", "python", "typescript", "java",
-      "kotlin", "c_sharp", "cpp", "rust", "clojure"
-    })
-  end,
+  opts = function(_, opts) vim.list_extend(opts.ensure_installed, {}) end,
 }
